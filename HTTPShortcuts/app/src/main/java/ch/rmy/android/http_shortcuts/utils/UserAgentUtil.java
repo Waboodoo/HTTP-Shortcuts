a22fa748dc736396af0bac823726cc211e0288c9
Roland Meyer <rmy@rmy.ch>
2017-05-19 20:00:13 +0200

    Fixed crash due to useragent

--- HTTPShortcuts/app/src/main/java/ch/rmy/android/http_shortcuts/utils/UserAgentUtil.java
+++ HTTPShortcuts/app/src/main/java/ch/rmy/android/http_shortcuts/utils/UserAgentUtil.java
@@ -5,10 +5,14 @@ import ch.rmy.android.http_shortcuts.BuildConfig;
 public class UserAgentUtil {
 
     public static String getUserAgent() {
+        String base = "HttpShortcuts/" + BuildConfig.VERSION_NAME;
         String userAgent = System.getProperty("http.agent");
         int start = userAgent.indexOf("(");
         int end = userAgent.indexOf(")");
-        return "HttpShortcuts/" + BuildConfig.VERSION_NAME + " " + userAgent.substring(start, end + 1);
+        if (start == -1 || end == -1 || start > end) {
+            return base;
+        }
+        return base + " " + userAgent.substring(start, end + 1);
     }
 
 }
