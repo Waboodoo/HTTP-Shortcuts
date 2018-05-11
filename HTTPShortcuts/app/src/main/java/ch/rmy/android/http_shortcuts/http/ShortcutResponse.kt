8328f8fe30cd2d402903c0bbf2c748577d39382f
Roland Meyer <rmy@rmy.ch>
2018-05-11 13:35:33 +0200

    Fixed handling of gzipped responses

--- HTTPShortcuts/app/src/main/java/ch/rmy/android/http_shortcuts/http/ShortcutResponse.kt
+++ HTTPShortcuts/app/src/main/java/ch/rmy/android/http_shortcuts/http/ShortcutResponse.kt
@@ -1,16 +1,45 @@
 package ch.rmy.android.http_shortcuts.http
 
 import com.android.volley.toolbox.HttpHeaderParser
+import java.io.BufferedReader
+import java.io.ByteArrayInputStream
+import java.io.InputStreamReader
 import java.nio.charset.Charset
+import java.util.zip.GZIPInputStream
+
 
 class ShortcutResponse internal constructor(val headers: Map<String, String>, val statusCode: Int, private val data: ByteArray) {
 
     val bodyAsString: String
         get() = try {
-            data.toString(Charset.forName(HttpHeaderParser.parseCharset(headers, "UTF-8")))
+            if (isGzipped()) {
+                parseGzippedByteArray()
+            } else {
+                parseByteArray()
+            }
         } catch (e: Exception) {
             String(data)
         }
+
+    private fun isGzipped(): Boolean = headers["Content-Encoding"] == "gzip"
+
+    private fun parseByteArray(): String =
+            data.toString(Charset.forName(HttpHeaderParser.parseCharset(headers, "UTF-8")))
+
+    private fun parseGzippedByteArray(): String {
+        val output = StringBuilder()
+        GZIPInputStream(ByteArrayInputStream(data)).use { gzipStream ->
+            InputStreamReader(gzipStream).use { reader ->
+                BufferedReader(reader, GZIP_BUFFER_SIZE).use { inStream ->
+                    while (true) {
+                        val line = inStream.readLine() ?: break
+                        output.append(line).append("\n")
+                    }
+                }
+            }
+        }
+        return output.toString()
+    }
 
     val contentType: String
         get() = if (headers.containsKey(HEADER_CONTENT_TYPE)) {
@@ -24,6 +53,8 @@ class ShortcutResponse internal constructor(val headers: Map<String, String>, va
         const val TYPE_TEXT = "text/plain"
         const val TYPE_XML = "text/xml"
         const val TYPE_JSON = "application/json"
+
+        private const val GZIP_BUFFER_SIZE = 16384
 
         private const val HEADER_CONTENT_TYPE = "Content-Type"
     }
