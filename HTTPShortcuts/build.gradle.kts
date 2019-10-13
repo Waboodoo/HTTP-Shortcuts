663695e5234fc4e54ccab9e5d485a15b90a50d3d
Roland Meyer <roland.meyer@beekeeper.io>
2019-10-13 12:27:01 +0200

    Updated Realm version

--- HTTPShortcuts/build.gradle.kts
+++ HTTPShortcuts/build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:3.4.1")
-        classpath("io.realm:realm-gradle-plugin:5.12.0")
+        classpath("io.realm:realm-gradle-plugin:6.0.0")
         classpath(kotlin("gradle-plugin", "1.3.50"))
     }
 }
