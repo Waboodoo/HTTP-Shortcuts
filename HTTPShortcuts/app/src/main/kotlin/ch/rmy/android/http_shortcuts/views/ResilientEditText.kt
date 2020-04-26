8efcb742f4ec728c00c4b04b8faff14270801f1f
Roland Meyer <roland.meyer@beekeeper.io>
2020-04-26 21:45:53 +0200

    Bugfixing time

--- HTTPShortcuts/app/src/main/kotlin/ch/rmy/android/http_shortcuts/views/ResilientEditText.kt
+++ HTTPShortcuts/app/src/main/kotlin/ch/rmy/android/http_shortcuts/views/ResilientEditText.kt
@@ -3,9 +3,14 @@ package ch.rmy.android.http_shortcuts.views
 import android.content.Context
 import android.util.AttributeSet
 import android.view.MotionEvent
+import androidx.appcompat.R
 import androidx.appcompat.widget.AppCompatEditText
 
-class ResilientEditText @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : AppCompatEditText(context, attrs, defStyleAttr) {
+class ResilientEditText @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = R.attr.editTextStyle
+) : AppCompatEditText(context, attrs, defStyleAttr) {
 
     override fun dispatchTouchEvent(event: MotionEvent?): Boolean =
         try {
