99fe081d4eed0e621d69468bf876ccc567684b4f
Roland Meyer <roland.meyer@beekeeper.io>
2020-05-03 11:23:31 +0200

    Configure Bugsnag for minification

--- HTTPShortcuts/build.gradle.kts
+++ HTTPShortcuts/build.gradle.kts
@@ -6,6 +6,7 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:3.5.2")
         classpath("io.realm:realm-gradle-plugin:6.1.0")
+        classpath("com.bugsnag:bugsnag-android-gradle-plugin:4.+")
         classpath(kotlin("gradle-plugin", "1.3.61"))
     }
 }
