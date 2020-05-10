0c79aceae0589c36515ce8b29108f324ab083a5a
Roland Meyer <roland.meyer@beekeeper.io>
2020-05-10 17:14:22 +0200

    Fixed changing language back to system settings

--- HTTPShortcuts/app/src/main/kotlin/ch/rmy/android/http_shortcuts/utils/LocaleHelper.kt
+++ HTTPShortcuts/app/src/main/kotlin/ch/rmy/android/http_shortcuts/utils/LocaleHelper.kt
@@ -7,27 +7,43 @@ import java.util.Locale
 
 object LocaleHelper {
 
+    private var deviceLocale: Locale? = null
+
     fun applyLocale(context: Context): Context {
-        return setLocale(context, getPersistedLocale(context) ?: return context)
+        if (deviceLocale == null) {
+            deviceLocale = Locale.getDefault()
+        }
+        val preferredLocale = getPersistedLocale(context)
+            ?.let(::getLocale)
+            ?: run {
+                if (deviceLocale == Locale.getDefault()) {
+                    return context
+                }
+                deviceLocale!!
+            }
+        return setLocale(context, preferredLocale)
     }
 
     private fun getPersistedLocale(context: Context): String? =
         Settings(context).language
 
-    private fun setLocale(context: Context, localeSpec: String): Context {
-        val localeParts = localeSpec.split('-')
-        val language = localeParts[0]
-        val country = localeParts.getOrNull(1)
-        val locale = if (country != null) {
-            Locale(language, country)
-        } else {
-            Locale(language)
-        }
+    private fun setLocale(context: Context, locale: Locale): Context {
         Locale.setDefault(locale)
         return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
             updateResources(context, locale)
         } else {
             updateResourcesLegacy(context, locale)
+        }
+    }
+
+    private fun getLocale(localeSpec: String): Locale {
+        val localeParts = localeSpec.split('-')
+        val language = localeParts[0]
+        val country = localeParts.getOrNull(1)
+        return if (country != null) {
+            Locale(language, country)
+        } else {
+            Locale(language)
         }
     }
 
