4654e9b920251c9b69025904759bb91d495cc0a6
Roland Meyer <roland.meyer@beekeeper.io>
2020-06-03 11:21:52 +0200

    Updated tooling

--- HTTPShortcuts/build.gradle.kts
+++ HTTPShortcuts/build.gradle.kts
@@ -4,8 +4,8 @@ buildscript {
         google()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:3.6.0")
-        classpath("io.realm:realm-gradle-plugin:6.1.0")
+        classpath("com.android.tools.build:gradle:4.0.0")
+        classpath("io.realm:realm-gradle-plugin:7.0.0")
         classpath("com.bugsnag:bugsnag-android-gradle-plugin:4.+")
         classpath(kotlin("gradle-plugin", "1.3.72"))
     }
@@ -20,12 +20,14 @@ ext {
     set("versionName", "$versionMajor.$versionMinor.$versionPatch")
     set("versionCode", ((((versionMajor.toInt()) * 1000 + versionMinor.toInt()) * 1000 + versionPatch.toInt()) * 1000) + versionBuild.toInt())
     set("bugsnagAPIKey", System.getenv("BUGSNAG_API_KEY") ?: "")
+    set("buildTimestamp", java.util.Date().time.toString())
+
+    // Encryption keys only used for legacy migrations, will hopefully be removed soon
     set("realmEncryptionKey", if (System.getenv("REALM_ENCRYPTION_KEY") != null) {
         "Wr3DmyNj0[{(,8g%jX2{03P45_p`N6|2zX08poC7a96dL9,FR_9|Uw<2%]?3Ij)4"
     } else {
         "ZX06poC7a96dL9,FR_9|Ww<2%]?4Ij(3wR3DmyNj0[{(,8g%jX2{03P45_p`N6|2"
     })
-    set("buildTimestamp", java.util.Date().time.toString())
 }
 
 allprojects {
