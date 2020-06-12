68e374b3fbd4db5c9a3267d900e1b9077dbe65ee
Roland Meyer <rmy@rmy.ch>
2020-06-12 16:52:57 +0200

    Revert "Revert "add wifi ip type and change response to response""

--- HTTPShortcuts/app/src/main/kotlin/ch/rmy/android/http_shortcuts/variables/types/VariableTypeFactory.kt
+++ HTTPShortcuts/app/src/main/kotlin/ch/rmy/android/http_shortcuts/variables/types/VariableTypeFactory.kt
@@ -14,6 +14,7 @@ object VariableTypeFactory {
         Variable.TYPE_DATE -> DateType()
         Variable.TYPE_TIME -> TimeType()
         Variable.TYPE_SLIDER -> SliderType()
+        Variable.TYPE_WIFI_IP -> WIFIIPType()
         else -> ConstantType()
     }
 
