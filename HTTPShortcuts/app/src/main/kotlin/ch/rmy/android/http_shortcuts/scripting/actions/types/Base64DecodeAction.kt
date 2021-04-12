42e2e37d23401202cbd4baef0208e4f83fbba043
Roland Meyer <roland.meyer@beekeeper.io>
2021-04-12 18:30:46 +0200

    Handle Base64 decode error

--- HTTPShortcuts/app/src/main/kotlin/ch/rmy/android/http_shortcuts/scripting/actions/types/Base64DecodeAction.kt
+++ HTTPShortcuts/app/src/main/kotlin/ch/rmy/android/http_shortcuts/scripting/actions/types/Base64DecodeAction.kt
@@ -1,6 +1,8 @@
 package ch.rmy.android.http_shortcuts.scripting.actions.types
 
+import android.content.Context
 import android.util.Base64
+import ch.rmy.android.http_shortcuts.exceptions.UserException
 import ch.rmy.android.http_shortcuts.scripting.ExecutionContext
 import io.reactivex.Single
 
@@ -8,6 +10,13 @@ class Base64DecodeAction(private val encoded: String) : BaseAction() {
 
     override fun executeForValue(executionContext: ExecutionContext): Single<String> =
         Single.fromCallable {
-            String(Base64.decode(encoded, Base64.DEFAULT))
+            try {
+                String(Base64.decode(encoded, Base64.DEFAULT))
+            } catch (e: IllegalArgumentException) {
+                throw object : UserException() {
+                    override fun getLocalizedMessage(context: Context) =
+                        "Invalid Base64: $encoded"
+                }
+            }
         }
 }
