eb3026f716690ce35bd06ce5318c3263aaa33acc
Roland Meyer <roland.meyer@beekeeper.io>
2021-06-05 19:07:25 +0200

    Improved Realm initialization

--- HTTPShortcuts/app/src/main/kotlin/ch/rmy/android/http_shortcuts/data/RealmFactory.kt
+++ HTTPShortcuts/app/src/main/kotlin/ch/rmy/android/http_shortcuts/data/RealmFactory.kt
@@ -22,6 +22,7 @@ internal class RealmFactory {
 
         private const val DB_NAME = "shortcuts_db_v2"
         private var instance: RealmFactory? = null
+        private lateinit var configuration: RealmConfiguration
 
         fun init(context: Context) {
             if (instance != null) {
@@ -30,6 +31,7 @@ internal class RealmFactory {
 
             try {
                 Realm.init(context)
+                configuration = createConfiguration(context)
                 instance = RealmFactory()
                     .apply {
                         createRealm().use { realm ->
@@ -49,30 +51,29 @@ internal class RealmFactory {
         fun <T> withRealm(block: (realm: Realm) -> T): T =
             getInstance().createRealm().use(block)
 
-        private fun setupBase(context: Context, realm: Realm) {
-            val defaultCategoryName = context.getString(R.string.shortcuts)
-            realm.executeTransaction {
-                val defaultCategory = Category(defaultCategoryName)
-                defaultCategory.id = newUUID()
-
-                val newBase = Base().apply {
-                    categories = RealmList()
-                    variables = RealmList()
-                    categories.add(defaultCategory)
-                    version = DatabaseMigration.VERSION
-                }
-                it.copyToRealm(newBase)
-            }
-        }
-
-        private val configuration: RealmConfiguration by lazy {
+        private fun createConfiguration(context: Context): RealmConfiguration =
             RealmConfiguration.Builder()
                 .schemaVersion(DatabaseMigration.VERSION)
+                .allowWritesOnUiThread(true) // TODO: Refactor app such that this is no longer needed
                 .migration(DatabaseMigration())
+                .initialData { realm ->
+                    setupBase(context, realm)
+                }
                 .name(DB_NAME)
                 .build()
+
+        private fun setupBase(context: Context, realm: Realm) {
+            val defaultCategoryName = context.getString(R.string.shortcuts)
+            val defaultCategory = Category(defaultCategoryName)
+            defaultCategory.id = newUUID()
+
+            val newBase = Base().apply {
+                categories = RealmList()
+                variables = RealmList()
+                categories.add(defaultCategory)
+                version = DatabaseMigration.VERSION
+            }
+            realm.copyToRealm(newBase)
         }
-
     }
-
 }
