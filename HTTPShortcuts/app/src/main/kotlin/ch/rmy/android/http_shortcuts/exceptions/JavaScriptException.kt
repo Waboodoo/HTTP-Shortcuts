786152b71a73fa9c2e0a18962c8ea5b89637b76f
Roland Meyer <roland.meyer@beekeeper.io>
2021-09-09 18:22:54 +0200

    Include line number in JS error message (if available)

--- HTTPShortcuts/app/src/main/kotlin/ch/rmy/android/http_shortcuts/exceptions/JavaScriptException.kt
+++ HTTPShortcuts/app/src/main/kotlin/ch/rmy/android/http_shortcuts/exceptions/JavaScriptException.kt
@@ -5,13 +5,28 @@ import ch.rmy.android.http_shortcuts.R
 import org.json.JSONException
 import org.liquidplayer.javascript.JSException
 
-class JavaScriptException(override val message: String?) : UserException() {
+class JavaScriptException(override val message: String?, private val error: Exception? = null) : UserException() {
 
-    constructor(e: JSException) : this(e.message)
+    constructor(e: JSException) : this(e.message, e)
 
-    constructor(e: JSONException) : this(e.message)
+    constructor(e: JSONException) : this(e.message, e)
 
     override fun getLocalizedMessage(context: Context): String =
-        context.getString(R.string.error_js_pattern, message)
+        getLineNumber()
+            ?.let { lineNumber ->
+                context.getString(R.string.error_js_pattern_with_line_number, lineNumber, message)
+            }
+            ?: context.getString(R.string.error_js_pattern, message)
+
+    private fun getLineNumber(): Int? =
+        (error as? JSException)?.error
+            ?.stack()
+            ?.split("\n")
+            ?.getOrNull(1)
+            ?.trim(')')
+            ?.split(':')
+            ?.dropLast(1)
+            ?.lastOrNull()
+            ?.toInt()
 
 }
