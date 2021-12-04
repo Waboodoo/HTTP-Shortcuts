5fd2f8d33d1bb935fff49ae3f1a5208dbc1393cf
Roland Meyer <roland.meyer@beekeeper.io>
2021-12-04 11:52:03 +0100

    Fixed wait() action deadlock when used multiple times

--- HTTPShortcuts/app/src/main/kotlin/ch/rmy/android/http_shortcuts/scripting/actions/types/WaitAction.kt
+++ HTTPShortcuts/app/src/main/kotlin/ch/rmy/android/http_shortcuts/scripting/actions/types/WaitAction.kt
@@ -2,11 +2,12 @@ package ch.rmy.android.http_shortcuts.scripting.actions.types
 
 import ch.rmy.android.http_shortcuts.scripting.ExecutionContext
 import io.reactivex.Completable
+import io.reactivex.schedulers.Schedulers
 import java.util.concurrent.TimeUnit
 
 class WaitAction(private val duration: Int) : BaseAction() {
 
     override fun execute(executionContext: ExecutionContext): Completable =
-        Completable.complete().delay(duration.toLong(), TimeUnit.MILLISECONDS)
+        Completable.timer(duration.toLong(), TimeUnit.MILLISECONDS, Schedulers.single())
 
 }
