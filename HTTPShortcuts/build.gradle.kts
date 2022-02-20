a433035afe29ba742e8d85446a20054fb6601c99
Roland Meyer <roland.meyer@beekeeper.io>
2022-02-20 18:48:47 +0100

    Bumped some version numbers

--- HTTPShortcuts/build.gradle.kts
+++ HTTPShortcuts/build.gradle.kts
@@ -8,7 +8,7 @@ buildscript {
         classpath("io.realm:realm-gradle-plugin:10.8.1")
         classpath("com.bugsnag:bugsnag-android-gradle-plugin:7.1.0")
         classpath("org.jetbrains:markdown:0.1.45")
-        classpath(kotlin("gradle-plugin", "1.6.0"))
+        classpath(kotlin("gradle-plugin", "1.6.10"))
     }
 }
 
