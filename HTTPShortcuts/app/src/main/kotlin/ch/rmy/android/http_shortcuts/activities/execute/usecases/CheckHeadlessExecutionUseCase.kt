25cba68120071439969a066e7cc127eaec7ee486
Roland Meyer <rmy@rmy.ch>
2022-11-08 14:04:20 +0100

    Improved headless execution for Android 13

--- HTTPShortcuts/app/src/main/kotlin/ch/rmy/android/http_shortcuts/activities/execute/usecases/CheckHeadlessExecutionUseCase.kt
+++ HTTPShortcuts/app/src/main/kotlin/ch/rmy/android/http_shortcuts/activities/execute/usecases/CheckHeadlessExecutionUseCase.kt
@@ -4,16 +4,21 @@ import ch.rmy.android.http_shortcuts.data.models.ResponseHandlingModel.Companion
 import ch.rmy.android.http_shortcuts.data.models.ResponseHandlingModel.Companion.SUCCESS_OUTPUT_NONE
 import ch.rmy.android.http_shortcuts.data.models.ResponseHandlingModel.Companion.UI_TYPE_TOAST
 import ch.rmy.android.http_shortcuts.data.models.ShortcutModel
+import ch.rmy.android.http_shortcuts.utils.PermissionManager
 import javax.inject.Inject
 
 class CheckHeadlessExecutionUseCase
 @Inject
-constructor() {
+constructor(
+    private val permissionManager: PermissionManager,
+) {
     operator fun invoke(shortcut: ShortcutModel): Boolean {
         val responseHandling = shortcut.responseHandling ?: return false
         val usesNoOutput = responseHandling.successOutput == SUCCESS_OUTPUT_NONE && responseHandling.failureOutput == FAILURE_OUTPUT_NONE
         val usesToastOutput = responseHandling.uiType == UI_TYPE_TOAST
         val usesCodeAfterExecution = shortcut.codeOnSuccess.isNotEmpty() || shortcut.codeOnFailure.isNotEmpty()
-        return (usesNoOutput || usesToastOutput) && !usesCodeAfterExecution && !shortcut.isWaitForNetwork
+        return (usesNoOutput || (usesToastOutput && permissionManager.hasNotificationPermission())) &&
+            !usesCodeAfterExecution &&
+            !shortcut.isWaitForNetwork
     }
 }
--- HTTPShortcuts/app/src/main/kotlin/ch/rmy/android/http_shortcuts/utils/PermissionManager.kt
+++ HTTPShortcuts/app/src/main/kotlin/ch/rmy/android/http_shortcuts/utils/PermissionManager.kt
@@ -1,7 +1,11 @@
 package ch.rmy.android.http_shortcuts.utils
 
 import android.Manifest.permission.ACCESS_FINE_LOCATION
+import android.Manifest.permission.POST_NOTIFICATIONS
 import android.Manifest.permission.READ_EXTERNAL_STORAGE
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.core.app.ActivityCompat
 import com.markodevcic.peko.Peko
 import com.markodevcic.peko.PermissionResult
@@ -9,7 +13,9 @@ import javax.inject.Inject
 
 class PermissionManager
 @Inject
-constructor() {
+constructor(
+    private val context: Context,
+) {
 
     @Inject
     lateinit var activityProvider: ActivityProvider
@@ -22,6 +28,13 @@ constructor() {
 
     fun shouldShowRationaleForLocationPermission(): Boolean =
         ActivityCompat.shouldShowRequestPermissionRationale(activityProvider.getActivity(), ACCESS_FINE_LOCATION)
+
+    fun hasNotificationPermission(): Boolean =
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            ActivityCompat.checkSelfPermission(context, POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED
+        } else {
+            true
+        }
 
     private suspend fun requestPermissionIfNeeded(permission: String): Boolean =
         Peko.requestPermissionsAsync(activityProvider.getActivity(), permission) is PermissionResult.Granted
