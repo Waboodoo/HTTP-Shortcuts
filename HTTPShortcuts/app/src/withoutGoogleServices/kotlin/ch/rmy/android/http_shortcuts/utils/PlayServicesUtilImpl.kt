9a1973cde6d58c8cdaafa0a92c74960dbc905208
Roland Meyer <rmy@rmy.ch>
2022-11-14 12:58:41 +0100

    Bugfix: fixed compile error in "release" build #305

--- HTTPShortcuts/app/src/withoutGoogleServices/kotlin/ch/rmy/android/http_shortcuts/utils/PlayServicesUtilImpl.kt
+++ HTTPShortcuts/app/src/withoutGoogleServices/kotlin/ch/rmy/android/http_shortcuts/utils/PlayServicesUtilImpl.kt
@@ -1,13 +1,12 @@
 package ch.rmy.android.http_shortcuts.utils
 
 import android.content.Context
-import ch.rmy.android.framework.utils.Destroyable
 
 class PlayServicesUtilImpl(@Suppress("UNUSED_PARAMETER") context: Context) : PlayServicesUtil {
     override fun isPlayServicesAvailable(): Boolean =
         false
 
-    override fun getLocation(onSuccess: (PlayServicesUtil.Location?) -> Unit, onError: (Exception) -> Unit): Destroyable {
+    override suspend fun getLocation(): PlayServicesUtil.Location? {
         throw NotImplementedError()
     }
 }
