8bfff962dbd6f030afd02f73931b8d1931be1527
Roland Meyer <rmy@rmy.ch>
2023-01-27 12:14:01 +0100

    Use NO_WRAP for Base64 encoding to avoid trailing newline characters #326

--- HTTPShortcuts/app/src/main/kotlin/ch/rmy/android/http_shortcuts/scripting/actions/types/Base64EncodeAction.kt
+++ HTTPShortcuts/app/src/main/kotlin/ch/rmy/android/http_shortcuts/scripting/actions/types/Base64EncodeAction.kt
@@ -6,5 +6,5 @@ import ch.rmy.android.http_shortcuts.scripting.ExecutionContext
 class Base64EncodeAction(private val text: ByteArray) : BaseAction() {
 
     override suspend fun execute(executionContext: ExecutionContext): String =
-        Base64.encodeToString(text, Base64.DEFAULT)
+        Base64.encodeToString(text, Base64.NO_WRAP)
 }
