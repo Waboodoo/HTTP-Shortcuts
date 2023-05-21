bdca6124506b20b8b04b620b3917a185f2040ea6
Roland Meyer <rmy@rmy.ch>
2023-05-21 16:35:53 +0200

    Factored out base URL from ExternalURLs

--- HTTPShortcuts/app/src/main/kotlin/ch/rmy/android/http_shortcuts/utils/ExternalURLs.kt
+++ HTTPShortcuts/app/src/main/kotlin/ch/rmy/android/http_shortcuts/utils/ExternalURLs.kt
@@ -2,21 +2,23 @@ package ch.rmy.android.http_shortcuts.utils
 
 object ExternalURLs {
 
-    const val SHORTCUTS_DOCUMENTATION = "https://http-shortcuts.rmy.ch/shortcuts"
-    const val CATEGORIES_DOCUMENTATION = "https://http-shortcuts.rmy.ch/categories"
-    const val VARIABLES_DOCUMENTATION = "https://http-shortcuts.rmy.ch/variables"
-    const val SCRIPTING_DOCUMENTATION = "https://http-shortcuts.rmy.ch/scripting#scripting"
-    const val IMPORT_EXPORT_DOCUMENTATION = "https://http-shortcuts.rmy.ch/import-export"
-    const val CERTIFICATE_PINNING_DOCUMENTATION = "https://http-shortcuts.rmy.ch/advanced#certificate-pinning"
+    private const val BASE_URL = "https://http-shortcuts.rmy.ch"
+    const val SHORTCUTS_DOCUMENTATION = "$BASE_URL/shortcuts"
+    const val CATEGORIES_DOCUMENTATION = "$BASE_URL/categories"
+    const val VARIABLES_DOCUMENTATION = "$BASE_URL/variables"
+    const val SCRIPTING_DOCUMENTATION = "$BASE_URL/scripting#scripting"
+    const val IMPORT_EXPORT_DOCUMENTATION = "$BASE_URL/import-export"
+    const val CERTIFICATE_PINNING_DOCUMENTATION = "$BASE_URL/advanced#certificate-pinning"
 
-    const val PRIVACY_POLICY = "https://http-shortcuts.rmy.ch/privacy-policy"
-    const val DOCUMENTATION_PAGE = "https://http-shortcuts.rmy.ch/documentation"
-    const val DONATION_PAGE = "https://http-shortcuts.rmy.ch/support-me#donate"
+    const val PRIVACY_POLICY = "$BASE_URL/privacy-policy"
+    const val DOCUMENTATION_PAGE = "$BASE_URL/documentation"
+    const val DONATION_PAGE = "$BASE_URL/support-me#donate"
     const val PLAY_STORE = "https://play.google.com/store/apps/details?id=ch.rmy.android.http_shortcuts"
     const val F_DROID = "https://f-droid.org/en/packages/ch.rmy.android.http_shortcuts/"
     const val GITHUB = "https://github.com/Waboodoo/HTTP-Shortcuts"
+    const val RELEASES = "$GITHUB/releases"
     const val TRANSLATION = "https://poeditor.com/join/project/8tHhwOTzVZ"
 
     fun getScriptingDocumentation(docRef: String) =
-        "https://http-shortcuts.rmy.ch/scripting#$docRef"
+        "$BASE_URL/scripting#$docRef"
 }
