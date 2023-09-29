544e368c7c7aa02d4381e97e6e2e60bf48e9ccdf
Roland Meyer <rmy@rmy.ch>
2023-09-29 12:48:37 +0200

    Fixed `openApp` function

--- HTTPShortcuts/app/src/main/kotlin/ch/rmy/android/http_shortcuts/scripting/actions/types/OpenAppAction.kt
+++ HTTPShortcuts/app/src/main/kotlin/ch/rmy/android/http_shortcuts/scripting/actions/types/OpenAppAction.kt
@@ -2,6 +2,8 @@ package ch.rmy.android.http_shortcuts.scripting.actions.types
 
 import android.content.ActivityNotFoundException
 import android.content.Context
+import android.content.IntentSender.SendIntentException
+import android.os.Build
 import ch.rmy.android.framework.extensions.startActivity
 import ch.rmy.android.http_shortcuts.R
 import ch.rmy.android.http_shortcuts.exceptions.ActionException
@@ -21,17 +23,24 @@ constructor(
         withContext(Dispatchers.Main) {
             try {
                 activityProvider.withActivity { activity ->
-                    context.packageManager.getLaunchIntentForPackage(packageName)
-                        ?.startActivity(activity)
-                        ?: throwUnsupportedError()
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                        context.packageManager.getLaunchIntentSenderForPackage(packageName)
+                            .sendIntent(activity, 0, null, null, null)
+                    } else {
+                        context.packageManager.getLaunchIntentForPackage(packageName)
+                            ?.startActivity(activity)
+                            ?: throwUnsupportedError(packageName)
+                    }
                 }
             } catch (e: ActivityNotFoundException) {
-                throwUnsupportedError()
+                throwUnsupportedError(packageName)
+            } catch (e: SendIntentException) {
+                throwUnsupportedError(packageName)
             }
         }
     }
 
-    private fun throwUnsupportedError(): Nothing {
+    private fun throwUnsupportedError(packageName: String): Nothing {
         throw ActionException {
             getString(R.string.error_no_app_found, packageName)
         }
