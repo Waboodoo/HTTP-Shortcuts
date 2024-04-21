cd4c5723bd0f572c24a7f34bac8dd2bb5477f8c6
Roland Meyer <rmy@rmy.ch>
2024-04-21 11:36:47 +0200

    Forego showing the changelog when no webview is installed

--- HTTPShortcuts/app/src/main/kotlin/ch/rmy/android/http_shortcuts/activities/main/usecases/ShouldShowChangeLogDialogUseCase.kt
+++ HTTPShortcuts/app/src/main/kotlin/ch/rmy/android/http_shortcuts/activities/main/usecases/ShouldShowChangeLogDialogUseCase.kt
@@ -1,6 +1,8 @@
 package ch.rmy.android.http_shortcuts.activities.main.usecases
 
+import android.webkit.WebView
 import androidx.annotation.CheckResult
+import ch.rmy.android.framework.extensions.logException
 import ch.rmy.android.http_shortcuts.utils.Settings
 import ch.rmy.android.http_shortcuts.utils.VersionUtil
 import javax.inject.Inject
@@ -15,6 +17,13 @@ constructor(
     operator fun invoke(): Boolean {
         val lastSeenVersion = settings.changeLogLastVersion
         if (isPermanentlyHidden) {
+            return false
+        }
+        try {
+            // Try to access a harmless method on the WebView. This will fail if no WebView is installed.
+            WebView.setWebContentsDebuggingEnabled(false)
+        } catch (e: Exception) {
+            logException(e)
             return false
         }
         val version = versionUtil.getVersionName()
