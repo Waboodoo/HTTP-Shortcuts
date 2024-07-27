f361e16c703f75dc7b4870d9d2dc229e553d57ff
Roland Meyer <rmy@rmy.ch>
2024-07-27 16:37:02 +0200

    Easy hack to allow English plurals in search

--- HTTPShortcuts/app/src/main/kotlin/ch/rmy/android/http_shortcuts/utils/SearchUtil.kt
+++ HTTPShortcuts/app/src/main/kotlin/ch/rmy/android/http_shortcuts/utils/SearchUtil.kt
@@ -8,6 +8,14 @@ object SearchUtil {
             .lowercase()
             .replace("[^a-z0-9]".toRegex(), " ")
             .split("\\s+".toRegex())
+            .flatMap {
+                // Best-effort hack to de-pluralize search terms. This will obviously only work well in English.
+                if (it.endsWith("s")) {
+                    listOf(it, it.dropLast(1))
+                } else {
+                    listOf(it)
+                }
+            }
             .filter { it.isNotBlank() && it.length >= minLength }
             .toSet()
 }
