44b005c6b879f68b4395f730f61b2572014810c5
Roland Meyer <rmy@rmy.ch>
2024-12-06 12:11:56 +0100

    Move repository configuration

--- HTTPShortcuts/build.gradle.kts
+++ HTTPShortcuts/build.gradle.kts
@@ -28,13 +28,6 @@ ext {
 }
 
 allprojects {
-    repositories {
-        mavenCentral()
-        maven("https://jitpack.io")
-        maven("https://oss.sonatype.org/content/repositories/snapshots/")
-        google()
-    }
-
     apply {
         plugin("com.diffplug.spotless")
     }
--- HTTPShortcuts/settings.gradle.kts
+++ HTTPShortcuts/settings.gradle.kts
@@ -1,3 +1,15 @@
 include(":app")
 include(":curl_command")
 include(":favicon_grabber")
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        // Only include repos allowed by F-Droid
+        // see https://f-droid.org/docs/Inclusion_Policy/
+        mavenCentral()
+        maven("https://jitpack.io")
+        maven("https://oss.sonatype.org/content/repositories/snapshots/")
+        google()
+    }
+}
