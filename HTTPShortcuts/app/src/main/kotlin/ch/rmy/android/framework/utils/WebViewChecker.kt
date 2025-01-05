4946b22ee40c3889f24f5e91411dbf89216412aa
Roland Meyer <rmy@rmy.ch>
2025-01-05 16:30:11 +0100

    Adjusted webview check

--- HTTPShortcuts/app/src/main/kotlin/ch/rmy/android/framework/utils/WebViewChecker.kt
+++ HTTPShortcuts/app/src/main/kotlin/ch/rmy/android/framework/utils/WebViewChecker.kt
@@ -1,14 +1,14 @@
 package ch.rmy.android.framework.utils
 
-import android.webkit.WebView
+import android.webkit.CookieManager
 
 object WebViewChecker {
     fun isWebViewAvailable(): Boolean =
         try {
             // Try to access a harmless method on the WebView. This will fail if no WebView is installed.
-            WebView.setWebContentsDebuggingEnabled(false)
+            CookieManager.getInstance()
             true
-        } catch (e: Exception) {
+        } catch (_: Exception) {
             false
         }
 }
