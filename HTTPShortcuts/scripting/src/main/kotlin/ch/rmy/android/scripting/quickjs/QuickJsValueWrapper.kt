c29bebe7455e820f5fecb0d7350eda9ab3426881
Roland Meyer <rmy@rmy.ch>
2025-01-17 10:59:00 +0100

    Preserve order of JS object properties

--- HTTPShortcuts/scripting/src/main/kotlin/ch/rmy/android/scripting/quickjs/QuickJsValueWrapper.kt
+++ HTTPShortcuts/scripting/src/main/kotlin/ch/rmy/android/scripting/quickjs/QuickJsValueWrapper.kt
@@ -4,7 +4,11 @@ import androidx.core.text.isDigitsOnly
 import ch.rmy.android.scripting.JsFunctionArgs
 import ch.rmy.android.scripting.JsFunctionArgsImpl
 import ch.rmy.android.scripting.JsValue
+import com.whl.quickjs.wrapper.JSArray
+import com.whl.quickjs.wrapper.JSFunction
+import com.whl.quickjs.wrapper.JSObject
 import com.whl.quickjs.wrapper.QuickJSArray
+import com.whl.quickjs.wrapper.QuickJSObject
 import org.json.JSONArray
 import org.json.JSONObject
 
@@ -17,11 +21,52 @@ internal class QuickJsValueWrapper(
             is String -> value
             is Map<*, *> -> JSONObject(value).toString()
             is List<*> -> JSONArray(value).toString()
+            is QuickJSObject -> value.stringify()
             else -> value.toString()
         }
 
     override fun asObject(): Map<String, Any?>? =
-        (value as? Map<String, Any?>)
+        value?.asMap()
+
+    private fun Any.asMap(): Map<String, Any?>? =
+        when (this) {
+            is Map<*, *> -> this.mapKeys { it.key.toString() }
+            is QuickJSObject -> this.asMap()
+            else -> null
+        }
+
+    private fun QuickJSObject.asMap(): Map<String, Any?>? {
+        // Using a custom implementation here instead of `toMap()`, as `toMap()` uses a HashMap which does not preserve the insertion order.
+        // N.B. that this custom implementation only preserves the order at the object's root level, not in its nested objects.
+        // It also doesn't support cyclic structures, but I hope my users don't do crazy stuff like that.
+        val jsObject = this
+        if (jsObject is JSArray) {
+            return null
+        }
+        val array = names
+        try {
+            return buildMap<String, Any?> {
+                for (i in 0 until array.length()) {
+                    val name = array.get(i) as String
+                    val value = jsObject.getProperty(name)
+                    when (value) {
+                        is JSFunction -> Unit
+                        is JSArray -> {
+                            put(name, value.toArray())
+                            value.release()
+                        }
+                        is JSObject -> {
+                            put(name, value.toMap())
+                            value.release()
+                        }
+                        else -> put(name, value)
+                    }
+                }
+            }
+        } finally {
+            array.release()
+        }
+    }
 
     override fun asByteArray(): ByteArray? =
         when {
@@ -33,19 +78,28 @@ internal class QuickJsValueWrapper(
         }
 
     override fun asListOfStrings(): List<String>? =
-        (value as? ArrayList<*>)?.map { it.toString() }
+        getValueAsList()
+            ?.map { it.toString() }
 
     override fun asListOfObjects(): List<Map<String, Any?>>? =
-        (value as? ArrayList<*>)
+        getValueAsList()
             ?.map {
-                (it as? Map<String, Any?>) ?: emptyMap()
+                it?.asMap() ?: emptyMap()
             }
 
-    override fun asJsFunctionArgs(): JsFunctionArgs? =
-        (value as? QuickJSArray)
-            ?.toArray()
-            ?.map {
-                QuickJsValueWrapper(it)
+    private fun getValueAsList(): List<Any?>? =
+        when (value) {
+            is List<*> -> value
+            is QuickJSArray -> buildList<Any?> {
+                for (i in 0 until value.length()) {
+                    add(value.get(i))
+                }
             }
+            else -> null
+        }
+
+    override fun asJsFunctionArgs(): JsFunctionArgs? =
+        getValueAsList()
+            ?.map(::QuickJsValueWrapper)
             ?.let(::JsFunctionArgsImpl)
 }
