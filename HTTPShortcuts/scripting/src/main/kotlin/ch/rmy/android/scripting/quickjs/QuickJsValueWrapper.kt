b8c290f057af4c3955313c2b6b3a2ee7380c6bbd
Roland Meyer <rmy@rmy.ch>
2025-01-21 10:26:24 +0100

    Updated QuickJs wrapper

--- HTTPShortcuts/scripting/src/main/kotlin/ch/rmy/android/scripting/quickjs/QuickJsValueWrapper.kt
+++ HTTPShortcuts/scripting/src/main/kotlin/ch/rmy/android/scripting/quickjs/QuickJsValueWrapper.kt
@@ -4,9 +4,6 @@ import androidx.core.text.isDigitsOnly
 import ch.rmy.android.scripting.JsFunctionArgs
 import ch.rmy.android.scripting.JsFunctionArgsImpl
 import ch.rmy.android.scripting.JsValue
-import com.whl.quickjs.wrapper.JSArray
-import com.whl.quickjs.wrapper.JSFunction
-import com.whl.quickjs.wrapper.JSObject
 import com.whl.quickjs.wrapper.QuickJSArray
 import com.whl.quickjs.wrapper.QuickJSObject
 import org.json.JSONArray
@@ -26,47 +23,14 @@ internal class QuickJsValueWrapper(
         }
 
     override fun asObject(): Map<String, Any?>? =
-        value?.asMap()
-
-    private fun Any.asMap(): Map<String, Any?>? =
-        when (this) {
-            is Map<*, *> -> this.mapKeys { it.key.toString() }
-            is QuickJSObject -> this.asMap()
+        when (value) {
+            is Map<*, *> -> value.mapKeysToString()
+            is QuickJSObject -> value.toMap(null, null, ::createObjectMap)
             else -> null
         }
 
-    private fun QuickJSObject.asMap(): Map<String, Any?>? {
-        // Using a custom implementation here instead of `toMap()`, as `toMap()` uses a HashMap which does not preserve the insertion order.
-        // N.B. that this custom implementation only preserves the order at the object's root level, not in its nested objects.
-        // It also doesn't support cyclic structures, but I hope my users don't do crazy stuff like that.
-        val jsObject = this
-        if (jsObject is JSArray) {
-            return null
-        }
-        val array = names
-        try {
-            return buildMap<String, Any?> {
-                for (i in 0 until array.length()) {
-                    val name = array.get(i) as String
-                    val value = jsObject.getProperty(name)
-                    when (value) {
-                        is JSFunction -> Unit
-                        is JSArray -> {
-                            put(name, value.toArray())
-                            value.release()
-                        }
-                        is JSObject -> {
-                            put(name, value.toMap())
-                            value.release()
-                        }
-                        else -> put(name, value)
-                    }
-                }
-            }
-        } finally {
-            array.release()
-        }
-    }
+    private fun Map<*, *>.mapKeysToString() =
+        mapKeys { (key, _) -> key.toString() }
 
     override fun asByteArray(): ByteArray? =
         when {
@@ -79,28 +43,26 @@ internal class QuickJsValueWrapper(
         }
 
     override fun asListOfStrings(): List<String>? =
-        getValueAsList()
-            ?.map { it.toString() }
+        asListOfAny()?.map { it.toString() }
 
     override fun asListOfObjects(): List<Map<String, Any?>>? =
-        getValueAsList()
+        asListOfAny()
             ?.map {
-                it?.asMap() ?: emptyMap()
+                ((it as? Map<*, *>)?.mapKeysToString()) ?: emptyMap()
             }
 
-    private fun getValueAsList(): List<Any?>? =
+    private fun asListOfAny(): List<Any?>? =
         when (value) {
             is List<*> -> value
-            is QuickJSArray -> buildList<Any?> {
-                for (i in 0 until value.length()) {
-                    add(value.get(i))
-                }
-            }
+            is QuickJSArray -> value.toArray(null, null, ::createObjectMap)
             else -> null
         }
 
+    private fun createObjectMap(): MutableMap<String, Any?> =
+        mutableMapOf()
+
     override fun asJsFunctionArgs(): JsFunctionArgs? =
-        (getValueAsList() ?: asObject()?.values)
+        (asListOfAny() ?: asObject()?.values)
             ?.map(::QuickJsValueWrapper)
             ?.let(::JsFunctionArgsImpl)
 }
