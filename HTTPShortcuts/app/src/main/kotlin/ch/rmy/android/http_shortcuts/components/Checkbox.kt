858a571ef4563de7b1a418b01c91e56205d27121
Roland Meyer <rmy@rmy.ch>
2025-02-27 21:35:13 +0100

    Removed unnecessary checkbox background color

--- HTTPShortcuts/app/src/main/kotlin/ch/rmy/android/http_shortcuts/components/Checkbox.kt
+++ HTTPShortcuts/app/src/main/kotlin/ch/rmy/android/http_shortcuts/components/Checkbox.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.selection.toggleable
 import androidx.compose.material3.ListItem
+import androidx.compose.material3.ListItemDefaults
 import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.Text
 import androidx.compose.material3.minimumInteractiveComponentSize
@@ -11,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.semantics.semantics
 
@@ -65,6 +67,7 @@ fun Checkbox(
                 )
             }
         },
+        colors = ListItemDefaults.colors(containerColor = Color.Transparent),
     )
 }
 
