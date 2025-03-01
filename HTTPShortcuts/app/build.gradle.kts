55fe5ec8fac009ec09ae460df4e0458688420a8f
Roland Meyer <rmy@rmy.ch>
2025-03-01 18:09:55 +0100

    Bumped version number to 3.27.0

--- HTTPShortcuts/app/build.gradle.kts
+++ HTTPShortcuts/app/build.gradle.kts
@@ -58,9 +58,9 @@ android {
         targetSdk = 35
 
         // Version name and code must remain as literals so that F-Droid can read them
-        versionName = "3.26.0"
+        versionName = "3.27.0"
         // 11,(2 digits major),(2 digits minor),(2 digits patch),(2 digits build)
-        versionCode = 1103260001
+        versionCode = 1103270000
 
         buildConfigField("String", "BUGSNAG_API_KEY", "\"$bugsnagAPIKey\"")
         buildConfigField("long", "BUILD_TIMESTAMP", buildTimestamp.toString())
