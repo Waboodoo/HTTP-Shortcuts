8c29dddc28288ae1e2eb1b55f1defb60b1f31d07
Roland Meyer <rmy@rmy.ch>
2025-11-02 21:10:07 +0100

    Move framework functionality into separate module

---
+++ HTTPShortcuts/framework/build.gradle.kts
@@ -0,0 +1,47 @@
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+}
+
+android {
+    namespace = "ch.rmy.android.framework"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 26
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        /* Used for F-Droid */
+        release {
+            ndk.debugSymbolLevel = "SYMBOL_TABLE"
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+
+        /* Used for Play Store & GitHub release page */
+        create("releaseFull") {
+            ndk.debugSymbolLevel = "SYMBOL_TABLE"
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlin {
+        jvmToolchain(11)
+    }
+}
+
+dependencies {
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.core)
+    implementation(libs.androidx.preference)
+    implementation(libs.androidx.annotation)
+    implementation(libs.kotlinx.coroutines.android)
+    implementation(libs.kotlinx.coroutines.jdk9)
+
+    testImplementation(libs.kotlin.test.junit5)
+}
