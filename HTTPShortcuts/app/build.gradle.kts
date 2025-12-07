afe688bedc22519cba523ec94c579c489a628af2
Roland Meyer <rmy@rmy.ch>
2025-12-07 12:26:10 +0100

    Bump version number to 4.1.0

--- HTTPShortcuts/app/build.gradle.kts
+++ HTTPShortcuts/app/build.gradle.kts
@@ -59,9 +59,9 @@ android {
         targetSdk = 35
 
         // Version name and code must remain as literals so that F-Droid can read them
-        versionName = "4.0.0"
+        versionName = "4.1.0"
         // 11,(2 digits major),(2 digits minor),(2 digits patch),(2 digits build)
-        versionCode = 1104000001
+        versionCode = 1104010000
 
         buildConfigField("String", "BUGSNAG_API_KEY", "\"$bugsnagAPIKey\"")
         buildConfigField("long", "BUILD_TIMESTAMP", buildTimestamp.toString())
