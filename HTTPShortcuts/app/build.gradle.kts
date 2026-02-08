649f14bd729ea39fdcfc479c295dcbbd79c81a59
Roland Meyer <rmy@rmy.ch>
2026-02-08 17:28:04 +0100

    Bumped build number

--- HTTPShortcuts/app/build.gradle.kts
+++ HTTPShortcuts/app/build.gradle.kts
@@ -64,7 +64,7 @@ android {
         // Version name and code must remain as literals so that F-Droid can read them
         versionName = "4.2.0"
         // 11,(2 digits major),(2 digits minor),(2 digits patch),(2 digits build)
-        versionCode = 1104020001
+        versionCode = 1104020002
 
         buildConfigField("String", "BUGSNAG_API_KEY", "\"$bugsnagAPIKey\"")
         buildConfigField("long", "BUILD_TIMESTAMP", buildTimestamp.toString())
